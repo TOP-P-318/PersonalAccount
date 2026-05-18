fd863510f8b0e76e66b3fd746c70e05aebc4b750
Alexandr Shamraev <alexandr.shamraev.github@gmail.com>
2026-05-18 18:55:14 +0300

    dgdf

--- Program.cs
+++ Program.cs
@@ -57,6 +57,8 @@ namespace PersonalAccount
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            
+            // asdlkasklfhasjkdgj
 
             app.UseHttpsRedirection();
             app.UseRouting();
